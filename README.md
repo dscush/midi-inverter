bb24de5a698c189cbc804ce9b70b6a80df5bb04a
Dan Cusher <dan@dancusher.com>
2017-02-18 07:29:57 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## Synopsis
+
+This is a web app that inverts all the pitches of a midi file, keeping the intervals the same, but their directions reversed.
+
+Inspired by [Andrew Huang](https://www.youtube.com/watch?v=4IAZY7JdSHU).
+
+## Contributors
+
+Dan Cusher
+dan at dan cusher dot com
+
+## License
+
+ISC License
+
+Copyright (c) 2017, Dan Cusher
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+from midi_inverter import midi_inverter
+from werkzeug.utils import secure_filename
+from flask import Flask, request, render_template, send_file, flash, url_for, redirect
+app = Flask(__name__)
+app.secret_key = 'Z$\x82l\xd7\x8fc?F\xaa\x1b\xc2U\x86\x9e)\xaa\x04#K\x94y\xb4\xf8'
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/invert', methods=['GET', 'POST'])
+def invert():
+    error = None
+    if request.method == 'POST':
+        f = request.files['midi_file']
+        inversion_type = int(request.form['inversionType'])
+        print request
+        print inversion_type
+        try:
+            inverted_midi = midi_inverter.invert_midi(f, inversion_type)
+        except IOError:
+            error = 'Invalid midi file'
+            flash('Invalid midi file - file could be corrupt, or simply not a midi file.')
+            return redirect(url_for('index'))
+        inverted_filename = os.path.splitext(secure_filename(f.filename))
+        inverted_filename = inverted_filename[0] + "--inverted" + (inverted_filename[1] or '.mid')
+        return send_file(inverted_midi, mimetype="audio/midi", as_attachment=True, attachment_filename=inverted_filename)
